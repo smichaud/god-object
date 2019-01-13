dafedce57636e9aed5514a5a4d6829997bba4346
Sebastien Michaud <sebastien.michaud.2@gmail.com>
2019-01-13 16:28:30 -0500

    WIP

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,14 +2,26 @@ import React from "react";
 import { render } from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
 import App from "./components/App";
 import rootReducer from "./reducers";
 
 const store = createStore(rootReducer);
+const theme = createMuiTheme({
+        palette: {
+        primary: blue,
+    },
+});
 
 render(
     <Provider store={store}>
-        <App />
+        <MuiThemeProvider theme={theme}>
+            <App />
+        </MuiThemeProvider>
     </Provider>,
     document.getElementById("root")
 );
+
+
