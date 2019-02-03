881e1f3650be35a6e528d4eb886684790db31824
Sebastien Michaud <sebastien.michaud.2@gmail.com>
2019-02-03 14:39:39 -0500

    Version at the end of the day

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,34 +1,46 @@
 import React from "react";
 import { Fragment } from "react";
 
+const listStyle = {
+    "float": "left",
+};
+
 const selectedPhotoStyle = {
-    "margin-left": "30px",
-    "font-weight": "bold",
+    "fontSize": "120%",
+    "fontWeight": "bold",
+};
+
+const imageStyle = {
+    "float": "right",
+    "marginRight": "10%",
 };
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            photoBaseURL: "http://127.0.0.1:5000/photos/",
+            photoBaseURL: "http://172.16.20.100:5000/photos/",
             filenames: [],
-            currentPhoto: ""
+            currentPhoto: "0000-placeholder.jpg",
+            isSetLatest: true,
         };
         this.fetchFilenames = this.fetchFilenames.bind(this);
     }
 
     fetchFilenames() {
-        fetch("http://localhost:5000/api/photos/")
+        fetch("http://172.16.20.100:5000/api/photos/")
           .then(response => response.json())
           .then((data) => {
-              this.setState({ filenames: data.filenames })
-              this.setState({ currentPhoto: data.filenames[0] })
+              this.setState({ filenames: data.filenames });
+              if (this.state.isSetLatest) {
+                  this.setState({ currentPhoto: data.filenames[0] });
+              }
           })
     }
 
     componentDidMount() {
         this.fetchFilenames();
-        this.interval = setInterval(this.fetchFilenames, 5000);
+        this.interval = setInterval(this.fetchFilenames, 2000);
     }
 
     componentWillUnmount() {
@@ -38,16 +50,23 @@ class App extends React.Component {
     render() {
         return(
           <Fragment>
-              <img id="image" src={this.state.photoBaseURL + this.state.currentPhoto} alt="timeframe"/>
-              <ul>
+              <ul style={listStyle}>
                   {this.state.filenames.map((filename) => (
                       <li key={filename}
                           style={filename === this.state.currentPhoto ? selectedPhotoStyle : null}
-                          onClick={() => {this.setState({currentPhoto: filename})}}>
+                          onClick={() => {
+                              if (filename !== this.state.filenames[0]) {
+                                  this.setState({isSetLatest: false});
+                              } else {
+                                  this.setState({isSetLatest: true});
+                              }
+                              this.setState({currentPhoto: filename});
+                          }}>
                           {filename}
                       </li>
                   ))}
               </ul>
+              <img id="image" style={imageStyle} src={this.state.photoBaseURL + this.state.currentPhoto} alt="timeframe"/>
           </Fragment>
         )
     }
